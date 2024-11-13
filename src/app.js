8362bed5e5e9e72cd1e08e3a5ad0eb81a6a376f2
Marcos Juan Martos Prados <martospradosmarcos@gmail.com>
2024-11-13 16:24:38 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  let number = 10
-  
-  console.log("Hello Rigo from the console!");
-  console.log("Hola Spain 91");
+  let nombre = ["Marcos", "Juan", "Laura", "María"];
+  let acción = ["comió", "estudió", "durmió", "apareció"];
+  let lugar = ["en la playa", "en la calle", "en el monte", "en la carretera"];
+  let cuándo = ["ayer", "hoy", "la semana pasada", "el lunes"];
+  let indiceNombre = Math.floor(Math.random() * nombre.length);
+  let indiceAcción = Math.floor(Math.random() * acción.length);
+  let indiceLugar = Math.floor(Math.random() * lugar.length);
+  let indiceCuándo = Math.floor(Math.random() * cuándo.length);
+  document.querySelector("#excuse").innerHTML =
+  nombre[indiceNombre] + " " + acción[indiceAcción] + " " + lugar[indiceLugar] + " " + cuándo[indiceCuándo];
 };
